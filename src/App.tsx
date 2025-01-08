f4d2d1c23b5e99d09181f4deb7464d5f3924b798
zccwebb <zenithcodecreations@gmail.com>
2025-01-09 02:18:19 +0530

    setup home page and create notes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,73 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Navigate, Route, Routes } from "react-router-dom"
+import NewNote from "./Pages/NewNote/NewNote"
+import useLocalStorage from "./componants/useLocalStorage/useLocalStorage"
+import { useMemo } from "react"
+import {v4 as uuidV4} from "uuid";
+import NoteList from "./componants/NoteList/NoteList"
+
+export type Note = {
+    id: string
+} & NoteData
+
+export type RawNote = {
+    id: string
+} & RawNoteData
+
+export type RawNoteData = {
+    title: string
+    markdown: string
+    tagIds: string[]
+}
+
+export type NoteData = {
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag={
+    id: string
+    label: string
+}
+
 function App() {
-  return <h1 className="text-4xl font-bold text-red-600">Hi</h1>
+    const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+    const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+    const notesWithTags = useMemo(()=>{
+        return notes.map(note => {
+            return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+        })
+    }, [notes, tags])
+
+    function onCreateNote({tags, ...data}: NoteData){
+        setNotes(prevNotes => {
+            return [
+                ...prevNotes,
+                { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+            ]
+        })
+    }
+
+    function addTag(tag: Tag){
+        setTags(prev=> [...prev, tag])
+    }
+
+  return(
+    <Container className="my-4">
+        <Routes>
+          <Route path="/" element={<NoteList availableTags={tags}/>}/>
+          <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>}/>
+          <Route path="/:id">
+              <Route index element={<h1 className="text-4xl font-bold text-red-600">Show</h1>}/>
+              <Route path="/edit" element={<h1 className="text-4xl font-bold text-red-600">Edit</h1>}/>
+          </Route>
+          <Route path="*" element={<Navigate to="/"/>}/>
+        </Routes>
+    </Container>
+  )
 }
 
 export default App
